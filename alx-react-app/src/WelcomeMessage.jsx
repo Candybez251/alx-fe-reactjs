c574e59a6e3c773a1c8d8a946e5840e351ca6273
candybez251 <kalkidanblemma@gmail.com>
2024-08-17 18:22:41 +0300

    modified and added app.jsx and WelcomeMessage.jsx

---
+++ alx-react-app/src/WelcomeMessage.jsx
@@ -0,0 +1,11 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Hello everyone, I am learning React at Alx!</h1>
+            <p>This is a simple JSX component.</p>
+            <p>I am learning about jsx!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
