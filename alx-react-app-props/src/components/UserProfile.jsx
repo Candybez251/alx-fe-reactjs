176f35797b6d93d259592da22f5a917530f00855
candybez251 <kalkidanblemma@gmail.com>
2024-08-31 19:16:47 +0300

    minor adjustment

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,9 @@
+import UserInfo from './UserInfo';
+import { useContext } from 'react';
+import UserContext from './UserContext';
+
+function ProfilePage() {
+  return <UserInfo />;
+}
+
+export default ProfilePage;
