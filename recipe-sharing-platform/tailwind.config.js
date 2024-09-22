d43ce274f27690ef430bcebd68ef34f494c4f243
candybez251 <kalkidanblemma@gmail.com>
2024-09-22 23:30:10 +0300

    added recipe sharing platform

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // older method
+  media: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
